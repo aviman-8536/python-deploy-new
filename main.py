e0c718c2c0e0d87b7d98ead26b5db537f44ad3e5
aviman <avimanm@gmail.com>
2024-07-25 13:22:39 +0530

    14commit

--- main.py
+++ main.py
@@ -8,14 +8,8 @@ numb2=5
 
 print("The product is: ",multiply(numb1,numb2))
  
- def addition(a, z):
-    return a * z
 
-def add(a, z):
-    return a + z
-
-numb1 = 15
-numb2 = 5
-
-print("The product is:", multiply(numb1, numb2))
-print("The sum is:", add(numb1, numb2))
+a=10
+b=5
+c=(a-b)
+print("substraction",c)
